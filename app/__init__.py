d07f887f5f072cc1b9ca16468fa816909c016f2e
Damien Nguyen <ngn.damien@gmail.com>
2023-01-14 15:40:25 +0100

    Restructure Flask code for more flexibility and add ticket names list

--- app/__init__.py
+++ app/__init__.py
@@ -16,15 +16,9 @@
 
 """Main for Flask app."""
 
-import csv
-import os
-import pprint
-from pathlib import Path
+from flask import Flask
 
-from flask import Flask, flash, redirect, render_template, request
-from werkzeug.utils import secure_filename
-
-from aluso_label.people import Person
+from .upload import upload_file
 
 
 def create_app(test_config=None):
@@ -37,78 +31,10 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    @app.route('/hello')
-    def hello():
-        return 'Hello World!'
-
-    @app.route('/upload', methods=['GET', 'POST'])
-    def upload_file():
-        _error = render_template('error.html'), {'Refresh': f'5; {request.url}'}
-
-        if request.method == 'POST':
-            if 'file' not in request.files:
-                flash('No file in request!')
-                return _error
-
-            file = request.files['file']
-            if file.filename == '':
-                flash('No selected file!', 'error')
-                return _error
-
-            filename = Path(file.filename)
-            if not file or filename.suffix not in ('.CSV', '.csv'):
-                flash('Uh-oh! Unsupported file extension and/or something unexpected happened!')
-                return _error
-
-            data_fields = {
-                'aluso_uid': ['ID user AF'],
-                'transation_uid': ['ID transaction/autorisation', 'Transaction/Authorization ID'],
-                'first_name': ['Prénom', 'First name'],
-                'last_name': ['Nom de famille', 'Family Name'],
-                'contribution_status': ['Cotisant', 'Contributor'],
-                'ticket_name': ['Nom du billet', 'Ticket name'],
-            }
-
-            people = []
-            for row in csv.DictReader(file.read().decode().splitlines(), skipinitialspace=True):
-                current_data = {}
-                for field, options in data_fields.items():
-                    for option in options:
-                        if option in row:
-                            current_data[field] = row[option].strip()
-                            break
-                    else:
-                        raise RuntimeError(f'Unable to parse CSV data for field {field} (tried {options})')
-                if not current_data['ticket_name']:
-                    # There are sometimes some double entries with identical data except empty ticket name
-                    continue
-
-                people.append(
-                    Person(
-                        current_data['aluso_uid'],
-                        current_data['first_name'],
-                        current_data['last_name'],
-                        current_data['contribution_status'] != '',
-                        current_data['contribution_status'] in ('Oui', 'Yes'),
-                        current_data['ticket_name'],
-                    )
-                )
-
-            return render_template('upload.html', filestring=pprint.pformat(people, indent=4))
-
-        return '''
-        <!doctype html>
-        <title>Upload new File</title>
-        <h1>Upload new File</h1>
-        <form method=post enctype=multipart/form-data>
-          <input type=file name=file>
-          <input type=submit value=Upload>
-        </form>
-        '''
+    app.add_url_rule('/', view_func=upload_file, methods=['GET', 'POST'])
 
     return app
+
+
+if __name__ == '__main__':
+    aluso_app = create_app()
---
+++ app/upload.py
@@ -0,0 +1,95 @@
+#   Copyright 2023 <Damien Nguyen>
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+#   documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+#   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+#   permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+#   Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+#   WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+#   OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+#   OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""CSV file upload utilities."""
+
+import csv
+import pprint
+from pathlib import Path
+
+from flask import flash, render_template, request
+
+from aluso_label.people import Person
+
+
+def upload_file():
+    """Convert an EPFL Alumni CSV file to a list of participants."""
+    _error = render_template('error.html'), {'Refresh': f'5; {request.url}'}
+
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file in request!')
+            return _error
+
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file!', 'error')
+            return _error
+
+        filename = Path(file.filename)
+        if not file or filename.suffix not in ('.CSV', '.csv'):
+            flash('Uh-oh! Unsupported file extension and/or something unexpected happened!')
+            return _error
+
+        data_fields = {
+            'aluso_uid': ['ID user AF'],
+            'transation_uid': ['ID transaction/autorisation', 'Transaction/Authorization ID'],
+            'first_name': ['Prénom', 'First name'],
+            'last_name': ['Nom de famille', 'Family Name'],
+            'contribution_status': ['Cotisant', 'Contributor'],
+            'ticket_name': ['Nom du billet', 'Ticket name'],
+        }
+
+        ticket_names = set()
+        people = []
+        for row in csv.DictReader(file.read().decode().splitlines(), skipinitialspace=True):
+            current_data = {}
+            for field, options in data_fields.items():
+                for option in options:
+                    if option in row:
+                        current_data[field] = row[option].strip()
+                        break
+                else:
+                    raise RuntimeError(f'Unable to parse CSV data for field {field} (tried {options})')
+            if not current_data['ticket_name']:
+                # There are sometimes some double entries with identical data except empty ticket name
+                continue
+
+            ticket_names.add(current_data['ticket_name'])
+            people.append(
+                Person(
+                    current_data['aluso_uid'],
+                    current_data['first_name'],
+                    current_data['last_name'],
+                    current_data['contribution_status'] != '',
+                    current_data['contribution_status'] in ('Oui', 'Yes'),
+                    current_data['ticket_name'],
+                )
+            )
+
+        ticket_names = sorted(ticket_names)
+        return render_template(
+            'upload.html', people_data=pprint.pformat(people, indent=4), ticket_list=str(ticket_names)
+        )
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
