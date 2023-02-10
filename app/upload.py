5a7cc3d25a16842dd197d3e330e59e853757cc6c
Damien Nguyen <ngn.damien@gmail.com>
2023-02-10 18:54:51 +0100

    Port upload page from Flask to FastAPI

--- app/upload.py
+++ app/upload.py
@@ -16,26 +16,89 @@
 """CSV file upload utilities."""
 
 import csv
+from collections.abc import Iterable
+from pathlib import Path
 
-from flask import redirect, render_template, session, url_for
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileAllowed, FileRequired
+from fastapi import APIRouter, Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+from starlette.datastructures import UploadFile
+from starlette_wtf import StarletteForm
 from wtforms import FileField, SubmitField
+from wtforms.validators import DataRequired, StopValidation
 
 from aluso_label.people import Person
 
+router = APIRouter()
+templates = Jinja2Templates(directory=Path(__file__).parent / 'templates')
 
-class CSVFileUploadForm(FlaskForm):
+
+class FileRequired(DataRequired):  # pylint: disable=too-few-public-methods
+    """Validates that the data is a Werkzeug :class:`~werkzeug.datastructures.FileStorage` object.
+
+    :param message: error message
+
+    You can also use the synonym ``file_required``.
+    """
+
+    def __call__(self, form, field):
+        """Call operator."""
+        if not (isinstance(field.data, UploadFile) and field.data):
+            raise StopValidation(self.message or field.gettext("This field is required."))
+
+
+class FileAllowed:  # pylint: disable=too-few-public-methods
+    """Validates that the uploaded file is allowed by a given list of extensions or a Flask-Uploads.
+
+    :param upload_set: A list of extensions or an
+        :class:`~flaskext.uploads.UploadSet`
+    :param message: error message
+
+    You can also use the synonym ``file_allowed``.
+    """
+
+    def __init__(self, upload_set, message=None):
+        """Initialise a FileAllowed instance."""
+        self.upload_set = upload_set
+        self.message = message
+
+    def __call__(self, form, field):
+        """Call operator."""
+        if not (isinstance(field.data, UploadFile) and field.data):
+            return
+
+        filename = field.data.filename.lower()
+        if not filename:
+            raise StopValidation('No file specified')
+
+        if isinstance(self.upload_set, Iterable):
+            if any(filename.endswith("." + x) for x in self.upload_set):
+                return
+
+            raise StopValidation(
+                self.message
+                or field.gettext("File does not have an approved extension: {extensions}").format(
+                    extensions=", ".join(self.upload_set)
+                )
+            )
+
+        if not self.upload_set.file_allowed(field.data, filename):
+            raise StopValidation(self.message or field.gettext("File does not have an approved extension."))
+
+
+class CSVFileUploadForm(StarletteForm):
     """CSV file input form."""
 
     csv_file = FileField(label='CSV File', validators=[FileRequired(), FileAllowed(['csv', 'CSV'])])
     submit = SubmitField(label='Upload')
 
 
-def upload_file():
+@router.route('/', methods=['GET', 'POST'])
+async def upload_file(request: Request):
     """Convert an EPFL Alumni CSV file to a list of participants."""
-    form = CSVFileUploadForm()
-    if form.validate_on_submit():
+    form = await CSVFileUploadForm.from_formdata(request)
+    if await form.validate_on_submit():
         data_fields = {
             'aluso_uid': ['ID user AF'],
             'first_name': ['Prénom', 'First name'],
@@ -46,7 +109,8 @@ def upload_file():
 
         ticket_names = set()
         people = []
-        for row in csv.DictReader(form.csv_file.data.read().decode().splitlines(), skipinitialspace=True):
+        csv_data = await form.csv_file.data.read()
+        for row in csv.DictReader(csv_data.decode().splitlines(), skipinitialspace=True):
             current_data = {}
             for field, options in data_fields.items():
                 for option in options:
@@ -68,7 +132,9 @@ def upload_file():
                         ),
                         ('li', options),
                     ]
-                    return render_template('upload.html', form=form, errors=errors)
+                    return templates.TemplateResponse(
+                        'upload.html', {'request': request, 'form': form, 'errors': errors}
+                    )
 
             if not current_data['ticket_name']:
                 # There are sometimes some double entries with identical data except empty ticket name
@@ -83,14 +149,11 @@ def upload_file():
                     current_data['contribution_status'] != '',
                     current_data['contribution_status'] in ('Oui', 'Yes'),
                     current_data['ticket_name'],
-                )
+                ).to_pydantic()
             )
 
-        ticket_names = sorted(ticket_names)
-
-        session['people'] = people
-        session['ticket_names'] = ticket_names
-        return redirect(url_for('process_people_list'))
+        request.session.update({'people': jsonable_encoder(people), 'ticket_names': sorted(ticket_names)})
+        return RedirectResponse(request.url_for('process_people_list_get'))
 
     # Something wrong happened -> resubmit current page
-    return render_template('upload.html', form=form)
+    return templates.TemplateResponse('upload.html', {'request': request, 'form': form})
